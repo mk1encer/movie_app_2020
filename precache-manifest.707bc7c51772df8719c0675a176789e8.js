94b49fbed2c7f7885638af34aeb3fc0924c5e142
Minseok Kim <minseokkim@Minseokui-MacBook-Pro.local>
2020-09-03 21:57:00 +0900

    Updates

---
+++ precache-manifest.707bc7c51772df8719c0675a176789e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f61cc9cefa5d4c243ca5f76bd89f82e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f0eaf362c24fa5f8b75e",
+    "url": "/movie_app_2020/static/css/main.7a81564d.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0eaf362c24fa5f8b75e",
+    "url": "/movie_app_2020/static/js/main.37550573.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
